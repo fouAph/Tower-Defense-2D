650e7623008c4a714484b44c64cb60fcf40b55a1
fouAph <37921661+fouAph@users.noreply.github.com>
2023-07-15 14:48:45 +0700

    Fix DetectEnemy(), now detect the enemy properly

--- Assets/Script/Tower.cs
+++ Assets/Script/Tower.cs
@@ -7,6 +7,7 @@ public class Tower : MonoBehaviour
 {
     public TowerStatsSO towerStatsSO;
     [SerializeField] Transform shootPoint;
+    [SerializeField] Transform towerWeaponHolder;
     [SerializeField] Transform projectilePrefab;
     [SerializeField] LayerMask enemyLayer;
 
@@ -44,8 +45,9 @@ public class Tower : MonoBehaviour
     void Update()
     {
         DetectEnemies();
-        if (enemyTarget && enemyCount > 0)
+        if (enemyTarget )
         {
+            RotateTowerWeaponToTarget();
             if (Time.time - lastFired > 1f / fireRate)
             {
                 lastFired = Time.time;
@@ -61,16 +63,51 @@ public class Tower : MonoBehaviour
         bp.SetupBullet(enemyTarget, projectileMoveSpeed, weaponDamage);
     }
 
-    public void DetectEnemies()
+    private void RotateTowerWeaponToTarget()
     {
-        // Collider2D[] 
-        enemyCount = Physics2D.OverlapCircleNonAlloc(transform.position + sensorOffset, sensorRadius, results, enemyLayer);
+        // Calculate the direction from the object's position to the target position
+        Vector3 direction = enemyTarget.transform.position - towerWeaponHolder.position;
 
-        if (enemyCount > 0)
-        {
-            enemyTarget = results[0].GetComponent<Enemy>();
-        }
+
+        // Calculate the angle in radians
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        // Set the rotation of the object
+        towerWeaponHolder.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
     }
+
+   public void DetectEnemies()
+{
+    results = Physics2D.OverlapCircleAll(transform.position + sensorOffset, sensorRadius, enemyLayer);
+
+    if (results.Length > 0)
+    {
+        float closestDistance = float.MaxValue;
+        Enemy closestEnemy = null;
+
+        foreach (Collider2D enemyCollider in results)
+        {
+            Enemy enemy = enemyCollider.GetComponent<Enemy>();
+
+            if (enemy != null)
+            {
+                float distance = Vector2.Distance(transform.position, enemy.transform.position);
+
+                if (distance < closestDistance)
+                {
+                    closestDistance = distance;
+                    closestEnemy = enemy;
+                }
+            }
+        }
+
+        enemyTarget = closestEnemy;
+    }
+    else
+    {
+        enemyTarget = null; // No enemies detected, so clear the target
+    }
+}
 
     public float GetSensorRadius()
     {
@@ -131,6 +168,6 @@ public class Tower : MonoBehaviour
 
     public int GetCurrentTowerLevel()
     {
-        return currentTowerLevel-1;
+        return currentTowerLevel - 1;
     }
 }
